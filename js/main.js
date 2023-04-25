7f7cd21f1713f3ef9e867e8cf356e2bc066c2801
jeff-PC <jefferyfrederic@gmail.com>
2023-04-24 18:34:04 -0700

    added education and experience

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-window.onscroll = function() {scrollFunction()};
+
 
 function scrollFunction() {
-  if (document.body.scrollTop >= 80 || document.documentElement.scrollTop >= 80) {
-    document.getElementById("navbar").style = "backdrop-filter: blur(20px); height: 60px;";
-  } else {
-    document.getElementById("navbar").style = "backdrop-filter: none; height: 100px;";
-  }
+    let navbar =  document.getElementById("navbar");
+    if (document.documentElement.scrollTop >= 50) {
+        navbar.style = "padding-top: 10px; padding-bottom: 10px; backdrop-filter: blur(20px);";
+    } else {
+        navbar.style = "padding-top: 30px; padding-bottom: 30px; backdrop-filter: blur(0px);";
+    }
 }
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 50;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+  
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", scrollFunction);
